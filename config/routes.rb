767666d2751d62356bc060486d53502ef02faa3a
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-01-13 13:59:43 -0500

    Resourcing curate/deposits routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ CurateIdentityAndLocation::Application.routes.draw do
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
+  namespace :curate do
+    resources :deposits
+  end
 end

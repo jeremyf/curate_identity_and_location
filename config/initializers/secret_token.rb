1b76287219a736838f21a9d0c7c9cf03569c1514
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-01-13 10:45:44 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CurateIdentityAndLocation::Application.config.secret_key_base = '9a9a399b33c98684e15f5165a0371ce22d45f631cc0189d6eb8e371513738a0865097902eda35353b31874561c363732377aef7ceae7c2bb3d6f7234f26a98d1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CurateIdentityAndLocation::Application.config.session_store :cookie_store, key: '_curate_identity_and_location_session'

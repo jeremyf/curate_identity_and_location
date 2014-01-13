5af72bbbcda2b6017ba788355ab8b4b8f43cea1d
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-01-13 13:57:32 -0500

    Updating db schema with Curate::Work
    Ran script
    $ rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140113155027) do
+ActiveRecord::Schema.define(version: 20140113185629) do
+
+  create_table "academic_works", force: true do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "curate_works", force: true do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -31,5 +43,11 @@ ActiveRecord::Schema.define(version: 20140113155027) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "works", force: true do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
